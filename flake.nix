b51846ad4659798b942416d79e026c4fc505cacd
Dante Zulli <dantezulli2004@gmail.com>
2025-06-24 18:46:41 -0300

    move home.nix, remove readme, update path in flake.nix. FLAKES IGNORE FILES THAT AREN'T TRACKED BY GIT

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     # NixOS configuration entrypoint
     # Available through 'sudo nixos-rebuild switch --flake .#hostname'
     nixosConfigurations = {
-      lachata = nixpkgs.lib.nixosSystem {
+      "lachata" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
 	system = "x86_64-linux";
         modules = [
@@ -35,20 +35,10 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.dante = import ./home.nix;
+            home-manager.users."dante" = import ./home/home.nix;
           }
 	];
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    #homeConfigurations = {
-    #  "dante@lachata" = home-manager.lib.homeManagerConfiguration {
-    #    pkgs = nixpkgs.legacyPackages.x86_64-linux; 
-    #    extraSpecialArgs = {inherit inputs outputs;};
-    #    modules = [./home-manager/home.nix];
-    #  };
-    #};
   };
 }
