771ae330df6f30dcef5c85894f9a3f51bbd01a00
Dante Zulli <dantezulli2004@gmail.com>
2025-06-25 20:20:35 -0300

    remove default.nix in modules and change default.nix for hosts

--- hosts/lachata/default.nix
+++ hosts/lachata/default.nix
@@ -1,21 +1,43 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
-  getHardwareConfig = let
-    local = ./hardware-configuration.nix;
-    global = "/etc/nixos/hardware-configuration.nix";
-  in
-    if builtins.pathExists local then local
-    else if builtins.pathExists global then global
-    else throw "hardware-configuration.nix not found";
+  getHardwareConfig =
+    let
+      local = ./hardware-configuration.nix;
+      global = "/etc/nixos/hardware-configuration.nix";
+    in
+    if builtins.pathExists local then
+      local
+    else if builtins.pathExists global then
+      global
+    else
+      throw "hardware-configuration.nix not found";
 in
 
 {
   imports = [
     getHardwareConfig
-    ../../modules
+    ../../modules/system/packages.nix
+    ../../modules/system/zram.nix
+    ../../modules/system/keymap.nix
+    ../../modules/system/nix.nix
+    ../../modules/system/bootloader.nix
+    ../../modules/system/networking.nix
+    ../../modules/system/time-i18n.nix
+    ../../modules/security/polkit.nix
+    ../../modules/desktop/keyring.nix
+    ../../modules/desktop/sway.nix
+    ../../modules/desktop/sound.nix
+    ../../modules/desktop/display/ly.nix
+    ../../modules/users/dante.nix
   ];
 
   networking.hostName = "lachata";
+
   system.stateVersion = "25.05";
 }
