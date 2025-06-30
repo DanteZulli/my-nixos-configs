f7a34ccf362133b0171ea077457b70da897f50dd
Dante Zulli <dantezulli2004@gmail.com>
2025-06-30 20:10:52 -0300

    add default fonts config

--- modules/system/common/fonts.nix
+++ modules/system/common/fonts.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.symbols-only
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.caskaydia-cove
+      nerd-fonts.symbols-only
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        sansSerif = [ "CaskaydiaCove Nerd Font Mono" ];
+        serif = [ "CaskaydiaCove Nerd Font Mono" ];
+        monospace = [ "CaskaydiaCove Nerd Font Mono" ];
+      };
+    };
+  };
 }
