82c47965f4d6842a68da1015191d518c80a92818
Dante Zulli <dantezulli2004@gmail.com>
2025-07-01 13:27:17 -0300

    add waybar styling with rosepine :D

--- home/hypr/hypr.nix
+++ home/hypr/hypr.nix
@@ -50,7 +50,7 @@
 
       # Decoration
       decoration = {
-        rounding = 6;
+        rounding = 8;
         rounding_power = 2;
         active_opacity = 1.0;
         inactive_opacity = 1.0;
--- home/hypr/waybar.nix
+++ home/hypr/waybar.nix
@@ -10,15 +10,158 @@
       main = {
         layer = "top";
         position = "top";
-        height = 30;
-        modules-left = [ "hyprland/workspaces" ];
-        modules-right = [
-          "pulseaudio"
-          "network"
-          "tray"
+        height = 0;
+        margin = "0 0 0 0";
+        modules-left = [
           "clock"
         ];
+        modules-center = [
+          "hyprland/workspaces"
+        ];
+        modules-right = [
+          "network"
+          "pulseaudio"
+          "battery"
+          "tray"
+        ];
+        "hyprland/workspaces" = {
+          format = "{icon}";
+          format-icons = {
+            active = " ";
+            default = " ";
+          };
+        };
+        "tray" = {
+          spacing = 5;
+          tooltip = false;
+        };
+        "clock" = {
+          timezone = "America/Argentina/Buenos_Aires";
+          format = "󰥔  {:%H:%M}";
+          format-alt = "  {:%d/%m/%Y}";
+          tooltip = false;
+          interval = 60;
+        };
+        "battery" = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "B: {capacity}%";
+          format-charging = "B: {capacity}% Chr";
+          format-plugged = "B: {capacity}% Conn";
+          format-alt = "{time}";
+        };
+        "network" = {
+          tooltip = true;
+          rotate = 0;
+          format-wifi = "󰖩   {bandwidthDownBytes}  {bandwidthUpBytes}";
+          format-ethernet = "   {bandwidthDownBytes}  {bandwidthUpBytes}";
+          format-disconnected = "  󰌙  ";
+          tooltip-format = "󰩟 {ifname}: {gwaddr}";
+          tooltip-format-disconnected = "  󰌙  ";
+          interval = 5;
+        };
+        "pulseaudio" = {
+          scroll-step = 1;
+          max-volume = 130;
+          format = "  {volume}%";
+          format-bluetooth = " {volume}%";
+          format-muted = "󰝟  muted";
+          nospacing = 1;
+          on-click = "pavucontrol";
+          tooltip = false;
+        };
       };
     };
+    style = ''
+      @define-color base            #191724;
+      @define-color surface         #1f1d2e;
+      @define-color overlay         #26233a;
+
+      @define-color muted           #6e6a86;
+      @define-color subtle          #908caa;
+      @define-color text            #e0def4;
+
+      @define-color love            #eb6f92;
+      @define-color gold            #f6c177;
+      @define-color rose            #ebbcba;
+      @define-color pine            #31748f;
+      @define-color foam            #9ccfd8;
+      @define-color iris            #c4a7e7;
+
+      @define-color highlightLow    #21202e;
+      @define-color highlightMed    #403d52;
+      @define-color highlightHigh   #524f67;
+            
+
+      * {
+        border: none;
+        border-radius: 0;
+        margin: 0;
+        padding: 2px;
+        font-family: "UbuntuMono Nerd Font";
+        font-size: 13px;
+      }
+
+      window#waybar {
+        background: @surface;
+      }
+
+      #workspaces {
+        background-color: @base;
+        padding: 1px;
+        border: 1px solid @overlay;
+        border-radius: 8px;
+        color: @muted;
+      }
+
+      #workspaces button {
+        background-color: transparent;
+        padding: 0;
+        margin: 0;
+        border: none;
+        box-shadow: none;
+        border-radius: 0;
+        background-image: none;
+        color: @muted;
+      }
+
+      #workspaces button:hover {
+        color: @text;
+      }
+
+      #workspaces button.active {
+        color: @text;
+      }
+
+      #workspaces button.urgent {
+        background-color: @love;
+      }
+
+      #clock,
+      #battery,
+      #pulseaudio,
+      #network,
+      #tray {
+        background-color: @base;
+        padding: 4px 6px;
+        border: 1px solid @overlay;
+        border-radius: 8px;
+        color: @text;
+      }
+
+      #clock {
+        color: @gold;
+      }
+
+      #network {
+        color: @foam;
+      }
+
+      #pulseaudio {
+        color: @iris;
+      }
+    '';
   };
 }
