d8abe7f52e1d7a7b3f79664d8202ac9d1ed2ca96
Dante Zulli <dantezulli2004@gmail.com>
2025-06-30 21:27:10 -0300

    remove rounded corners because i hate them

--- home/hypr/hypr.nix
+++ home/hypr/hypr.nix
@@ -49,8 +49,8 @@
 
       # Decoration
       decoration = {
-        rounding = 8;
-        rounding_power = 2;
+        rounding = 0;
+        rounding_power = 0;
         active_opacity = 1.0;
         inactive_opacity = 1.0;
         shadow = {
