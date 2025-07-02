ea84c7a38dee5632fcb8f150cdf3e24250f8da60
Dante Zulli <dantezulli2004@gmail.com>
2025-07-02 15:07:20 -0300

    improve amd graphics nix file, add amdgpu kernel module and lact soft to manage

--- modules/hardware/graphics/amd.nix
+++ modules/hardware/graphics/amd.nix
@@ -1,6 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      amdvlk
+    ];
+    extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+    ];
+  };
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  environment.systemPackages = with pkgs; [ lact ];
+  systemd = {
+    packages = with pkgs; [ lact ];
+    services.lactd.wantedBy = [ "multi-user.target" ];
+  };
+
   services.xserver.videoDrivers = [ "amdgpu" ];
 }
