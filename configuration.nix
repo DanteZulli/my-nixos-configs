8bd4f0e7bf64751c3f676c996e1951c7b99e0de7
Dante Zulli <dantezulli2004@gmail.com>
2025-07-06 12:44:09 -0300

    add wlr portal

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,7 @@
     extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
+      pkgs.xdg-desktop-portal-wlr
       pkgs.xdg-desktop-portal-gtk
     ];
     config.common.default = "*";
