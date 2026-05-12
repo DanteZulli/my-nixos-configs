b056313b09cb246bc76174fba849a60a61f45c80
Dante Zulli <dantezulli2004@gmail.com>
2026-05-12 19:22:08 -0300

    update: home/shell/zellij.nix

--- home/shell/zellij.nix
+++ home/shell/zellij.nix
@@ -1,63 +1,45 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  ...
+}: {
   programs.zellij = {
     enable = true;
     package = pkgs.zellij;
 
     settings = {
-      theme = "default";
+      show_startup_tips_timer_in_milliseconds = 0;
     };
 
     extraConfig = ''
       keybinds clear-defaults=true {
           normal {
-              bind "Alt n" { NewPane; }
-              bind "Alt t" { NewTab; }
-              bind "Alt w" { CloseFocus; }
               bind "Alt h" "Alt Left" { MoveFocus "Left"; }
               bind "Alt j" "Alt Down" { MoveFocus "Down"; }
               bind "Alt k" "Alt Up" { MoveFocus "Up"; }
               bind "Alt l" "Alt Right" { MoveFocus "Right"; }
-              bind "Alt 1" { GoToTab 1; }
-              bind "Alt 2" { GoToTab 2; }
-              bind "Alt 3" { GoToTab 3; }
-              bind "Alt 4" { GoToTab 4; }
-              bind "Alt 5" { GoToTab 5; }
-              bind "Alt 6" { GoToTab 6; }
-              bind "Alt 7" { GoToTab 7; }
-              bind "Alt 8" { GoToTab 8; }
-              bind "Alt 9" { GoToTab 9; }
-              bind "Alt s" { SwitchToMode "scroll"; }
+              bind "Alt n" { NewPane; }
+              bind "Alt w" { CloseFocus; }
               bind "Alt f" { ToggleFloatingPanes; }
-              bind "Alt q" { Quit; }
+              bind "Alt p" { SwitchToMode "pane"; }
+              bind "Alt t" { SwitchToMode "tab"; }
+              bind "Alt r" { SwitchToMode "resize"; }
+              bind "Alt s" { SwitchToMode "scroll"; }
           }
           pane {
               bind "h" "Left" { MoveFocus "Left"; }
               bind "j" "Down" { MoveFocus "Down"; }
               bind "k" "Up" { MoveFocus "Up"; }
               bind "l" "Right" { MoveFocus "Right"; }
-              bind "p" { SwitchFocus; }
               bind "n" { NewPane; }
               bind "w" { CloseFocus; }
               bind "f" { ToggleFloatingPanes; }
-              bind "t" { SwitchToMode "tab"; }
-              bind "s" { SwitchToMode "scroll"; }
+              bind "p" { SwitchFocus; }
           }
           tab {
               bind "h" "Left" { GoToPreviousTab; }
               bind "l" "Right" { GoToNextTab; }
-              bind "j" "Down" { MoveTab "Right"; }
-              bind "k" "Up" { MoveTab "Left"; }
               bind "n" { NewTab; }
               bind "w" { CloseFocus; }
-              bind "1" { GoToTab 1; }
-              bind "2" { GoToTab 2; }
-              bind "3" { GoToTab 3; }
-              bind "4" { GoToTab 4; }
-              bind "5" { GoToTab 5; }
-              bind "6" { GoToTab 6; }
-              bind "7" { GoToTab 7; }
-              bind "8" { GoToTab 8; }
-              bind "9" { GoToTab 9; }
           }
           resize {
               bind "h" "Left" { Resize "Left"; }
