93548b1be5d32666439079ae6a4b45fa8053d743
Dante Zulli <dantezulli2004@gmail.com>
2026-05-12 19:09:55 -0300

    update: home/programs/git.nix

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,6 +6,7 @@
       user.email = "dantezulli2004@gmail.com";
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+      alias.sync = "!git add . && git commit && git push";
     };
   };
 }
